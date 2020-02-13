11f30f675ccbcdb48100b5a36a733c97df01ed3f
Łukasz <32484818+lukigarazus@users.noreply.github.com>
2020-02-13 12:42:36 -0800

    Changes

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,23 @@
   <div id="app">
     <Layout>
       <Sider collapsible v-model="collapsed">
-        <Menu theme="dark" :defaultSelectedKeys="['1']" mode="inline">
-          <Item key="dash">
-            <Icon type="question-circle"/>
-            <span>Dashboard</span>
+        <Menu theme="dark" :defaultSelectedKeys="[tab]" mode="inline">
+          <Item v-for="item in menu" :key="item.value" @click="tab = item.value">
+            <Icon :type="item.icon"/>
+            <span>{{ item.label }}</span>
           </Item>
-          <Item key="create">
-            <Icon type="edit"/>
-            <span>Create a tree</span>
-          </Item>
-          <SubMenu key="trees" style="background: #10239e">
+          <SubMenu key="trees" style="background: #9e1068">
             <div slot="title">
               <Icon type="bars"/>
               <span>Trees</span>
             </div>
+            <Item v-if="!trees.length">
+              <span></span>
+              <span>No trees defined!</span>
+            </Item>
+            <Item v-for="tree in trees" :key="tree.name">
+              <span>{{ tree.name }}</span>
+            </Item>
           </SubMenu>
         </Menu>
       </Sider>
@@ -29,6 +32,7 @@
 <script>
 import "ant-design-vue/dist/antd.css";
 import { Layout, Menu, Icon } from "ant-design-vue";
+import Dexie from "dexie";
 import DecisionTree from "./components/DecisionTree";
 
 const { Content, Header, Sider } = Layout;
@@ -50,8 +54,20 @@ export default {
   data() {
     return {
       tab: "tree",
-      collapsed: true
+      collapsed: true,
+      menu: [
+        { label: "Dashboard", value: "dash", icon: "question-circle" },
+        { label: "Create a tree", value: "tree", icon: "edit" }
+      ],
+      db: new Dexie("decision"),
+      trees: []
     };
+  },
+  async created() {
+    this.db.version(1).stores({
+      trees: "++id, name, tree"
+    });
+    this.trees = await this.db.trees.toArray();
   }
 };
 </script>
